bb2c4deea88ea74d92a96c55c734591d259a6a29
mkiineko086 <49701911+mkiineko086@users.noreply.github.com>
2019-05-17 13:31:59 +0900

    前回と同じ

--- fukusyu0517/Form1.cs
+++ fukusyu0517/Form1.cs
@@ -16,5 +16,36 @@ namespace fukusyu0517
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 10;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 10;
+        }
     }
 }
